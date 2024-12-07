653623772a8125419926ba930ec786391627e5d8
noobkyle <kadangotinaishe5@gmail.com>
2024-12-07 13:09:02 -0600

    Main screen complete

--- wguSchedulingSoftware/MainScreen.Designer.cs
+++ wguSchedulingSoftware/MainScreen.Designer.cs
@@ -28,19 +28,226 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			label1 = new Label();
+			label2 = new Label();
+			label3 = new Label();
+			label4 = new Label();
+			button1 = new Button();
+			button2 = new Button();
+			button3 = new Button();
+			button4 = new Button();
+			button5 = new Button();
+			button6 = new Button();
+			button7 = new Button();
+			button8 = new Button();
+			button9 = new Button();
+			checkBox1 = new CheckBox();
+			checkBox2 = new CheckBox();
+			dataGridView1 = new DataGridView();
+			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
 			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label1.Location = new Point(382, 9);
+			label1.Name = "label1";
+			label1.Size = new Size(211, 50);
+			label1.TabIndex = 0;
+			label1.Text = "Dashboard";
+			label1.Click += label1_Click;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+			label2.Location = new Point(9, 117);
+			label2.Name = "label2";
+			label2.Size = new Size(175, 30);
+			label2.TabIndex = 1;
+			label2.Text = "Customer Access";
+			label2.Click += label2_Click;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+			label3.Location = new Point(223, 117);
+			label3.Name = "label3";
+			label3.Size = new Size(88, 30);
+			label3.TabIndex = 2;
+			label3.Text = "Reports";
+			label3.Click += label3_Click;
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+			label4.Location = new Point(588, 114);
+			label4.Name = "label4";
+			label4.Size = new Size(231, 30);
+			label4.TabIndex = 3;
+			label4.Text = "Appointment Calendar";
+			// 
+			// button1
+			// 
+			button1.Location = new Point(33, 163);
+			button1.Name = "button1";
+			button1.Size = new Size(90, 23);
+			button1.TabIndex = 4;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			button2.Location = new Point(33, 216);
+			button2.Name = "button2";
+			button2.Size = new Size(90, 23);
+			button2.TabIndex = 5;
+			button2.Text = "button2";
+			button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			button3.Location = new Point(33, 272);
+			button3.Name = "button3";
+			button3.Size = new Size(90, 23);
+			button3.TabIndex = 6;
+			button3.Text = "button3";
+			button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			button4.Location = new Point(176, 163);
+			button4.Name = "button4";
+			button4.Size = new Size(180, 23);
+			button4.TabIndex = 7;
+			button4.Text = "button4";
+			button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			button5.Location = new Point(176, 216);
+			button5.Name = "button5";
+			button5.Size = new Size(180, 23);
+			button5.TabIndex = 8;
+			button5.Text = "button5";
+			button5.UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			button6.Location = new Point(176, 272);
+			button6.Name = "button6";
+			button6.Size = new Size(180, 23);
+			button6.TabIndex = 9;
+			button6.Text = "button6";
+			button6.UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			button7.Location = new Point(549, 520);
+			button7.Name = "button7";
+			button7.Size = new Size(75, 23);
+			button7.TabIndex = 10;
+			button7.Text = "button7";
+			button7.UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			button8.Location = new Point(682, 520);
+			button8.Name = "button8";
+			button8.Size = new Size(75, 23);
+			button8.TabIndex = 11;
+			button8.Text = "button8";
+			button8.UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			button9.Location = new Point(821, 520);
+			button9.Name = "button9";
+			button9.Size = new Size(75, 23);
+			button9.TabIndex = 12;
+			button9.Text = "button9";
+			button9.UseVisualStyleBackColor = true;
+			button9.Click += button9_Click;
+			// 
+			// checkBox1
+			// 
+			checkBox1.AutoSize = true;
+			checkBox1.Location = new Point(588, 163);
+			checkBox1.Name = "checkBox1";
+			checkBox1.Size = new Size(83, 19);
+			checkBox1.TabIndex = 13;
+			checkBox1.Text = "checkBox1";
+			checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			checkBox2.AutoSize = true;
+			checkBox2.Location = new Point(746, 163);
+			checkBox2.Name = "checkBox2";
+			checkBox2.Size = new Size(83, 19);
+			checkBox2.TabIndex = 14;
+			checkBox2.Text = "checkBox2";
+			checkBox2.UseVisualStyleBackColor = true;
+			checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+			// 
+			// dataGridView1
+			// 
+			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridView1.Location = new Point(405, 184);
+			dataGridView1.Name = "dataGridView1";
+			dataGridView1.Size = new Size(608, 312);
+			dataGridView1.TabIndex = 15;
 			// 
 			// MainScreen
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
+			ClientSize = new Size(1041, 571);
+			Controls.Add(dataGridView1);
+			Controls.Add(checkBox2);
+			Controls.Add(checkBox1);
+			Controls.Add(button9);
+			Controls.Add(button8);
+			Controls.Add(button7);
+			Controls.Add(button6);
+			Controls.Add(button5);
+			Controls.Add(button4);
+			Controls.Add(button3);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Controls.Add(label4);
+			Controls.Add(label3);
+			Controls.Add(label2);
+			Controls.Add(label1);
 			Name = "MainScreen";
 			StartPosition = FormStartPosition.CenterScreen;
 			Text = "MainScreen";
+			Load += MainScreen_Load;
+			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
 			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private Label label1;
+		private Label label2;
+		private Label label3;
+		private Label label4;
+		private Button button1;
+		private Button button2;
+		private Button button3;
+		private Button button4;
+		private Button button5;
+		private Button button6;
+		private Button button7;
+		private Button button8;
+		private Button button9;
+		private CheckBox checkBox1;
+		private CheckBox checkBox2;
+		private DataGridView dataGridView1;
 	}
 }
--- wguSchedulingSoftware/MainScreen.cs
+++ wguSchedulingSoftware/MainScreen.cs
@@ -16,5 +16,35 @@ namespace wguSchedulingSoftware
 		{
 			InitializeComponent();
 		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void MainScreen_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void checkBox2_CheckedChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
